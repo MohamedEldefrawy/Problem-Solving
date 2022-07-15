678f0e27269ceda5414abe3f6abca5bda0f6eb1b
Mohamed Eldefrawy <mohamed.ahmed.aly.d@gmail.com>
2022-07-15 17:37:24 +0200

    Optimize time and space for solution of BestTimeToBuyAndSell

--- NeetCode/BestTimeToBuyAndSellAStock.py
+++ NeetCode/BestTimeToBuyAndSellAStock.py
@@ -1,33 +1,23 @@
 def solution():
     # prices = [7, 6, 5, 4, 3, 2]
     # prices = [1, 2]
-    prices = [1, 2, 4, 2, 5, 7, 2, 4, 9, 0, 9]
+    # prices = [1, 2, 4, 2, 5, 7, 2, 4, 9, 0, 9]
     # prices = [1, 2, 4]
     # prices = [7, 1, 5, 3, 6, 4]
     # prices = [0, 1, 0, 2, 1]
     # prices = [2, 1, 2, 0, 1]
     # prices = [3, 2, 6, 5, 0, 3]
-    # prices = [7, 5, 0, 0, 0, 0, 1, 5, 3, 6, 4, 1, 1, 1]
-    start = 0
-    results = []
-    if len(prices) > 2:
-        while start < len(prices) - 1:
-            checker = start + 1
-            while checker < len(prices):
-                if prices[start] >= prices[checker]:
-                    break
-                else:
-                    results.append(prices[checker] - prices[start])
-                    checker += 1
-            start += 1
-    elif len(prices) == 2:
-        if prices[0] < prices[1]:
-            return prices[1] - prices[0]
-    else:
-        return 0
-    if len(results) == 0:
-        return 0
-    return max(results)
+    prices = [7, 5, 0, 0, 0, 0, 1, 5, 3, 6, 4, 1, 1, 1]
+    buy = 0
+    sell = 1
+    profit = 0
+    while sell < len(prices):
+        if prices[buy] > prices[sell]:
+            buy = sell
+        else:
+            profit = max(profit, prices[sell] - prices[buy])
+        sell += 1
+    return profit
 
 
 print(solution())
