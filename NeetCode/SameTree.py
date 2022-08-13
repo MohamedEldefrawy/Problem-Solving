45dd89f16d2cdba226c76c3a4fda29f0404179ef
Mohamed Eldefrawy <mohamed.ahmed.aly.d@gmail.com>
2022-08-13 19:50:19 +0200

    Add cleaner solution

--- NeetCode/SameTree.py
+++ NeetCode/SameTree.py
@@ -22,6 +22,15 @@ def solve():
 
     same = False
 
+
+    # def isSameTree(p: TreeNode, q: TreeNode) -> bool:
+    #     if not p and not q:
+    #         return True
+    #     if p and q and p.val == q.val:
+    #         return isSameTree(p.left, q.left) and isSameTree(p.right, q.right)
+    #     else:
+    #         return False
+
     def dfs(first_root, second_root):
         if first_root and second_root:
             if first_root.val != second_root.val:
