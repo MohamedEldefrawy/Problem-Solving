49bf291d5ea3d27a9bb2abd8dd6735f849859c3f
Mohamed Eldefrawy <mohamed.ahmed.aly.d@gmail.com>
2022-09-07 21:44:14 +0200

    Solve MeetingRooms

---
+++ NeetCode/Intervals/MeetingRooms.py
@@ -0,0 +1,29 @@
+from typing import (
+    List,
+)
+
+
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+
+
+class Solution:
+    """
+    @param intervals: an array of meeting time intervals
+    @return: if a person could attend all meetings
+    """
+
+    def can_attend_meetings(self, intervals: List[Interval]) -> bool:
+        intervals.sort(key=lambda i: i.start)
+        i = 0
+        while i < len(intervals) - 1:
+            if intervals[i].end > intervals[i + 1].start:
+                return False
+            i += 1
+        return True
+
+
+test = Solution()
+print(test.can_attend_meetings([Interval(5, 8), Interval(6, 8)]))
